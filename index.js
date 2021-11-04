e0f2dd3cc3d8d4fedba45bb0d9cdf7cf959a1121
hoanbui29 <81216107+hoanbui29@users.noreply.github.com>
2021-11-04 20:53:14 +0700

    Update
    Update

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-document.addEventListener('keydown',e => {
-    e.preventDefault()
-    document.querySelector(`#${e.code}`).classList.remove('released')
-    document.querySelector(`#${e.code}`).classList.add('pressed')
-})
-document.addEventListener('keyup', e => {
-    e.preventDefault()
-    document.querySelector(`#${e.code}`).classList.remove('pressed')
-    document.querySelector(`#${e.code}`).classList.add('released')
-
-})
+const toggle = (event, isKeyDown) => {
+    event.preventDefault()
+    console.log(event.code)
+    const element = document.querySelector(`#${event.code}`)
+    isKeyDown?element.classList.add('key-down'):element.classList.remove('key-down')
+    isKeyDown?element.classList.remove('key-up'):element.classList.add('key-up')
+}
+document.addEventListener('keydown', e => toggle(e, true))
+document.addEventListener('keyup', e => toggle(e, false))
